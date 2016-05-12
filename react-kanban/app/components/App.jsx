4956b44317b6343ee417b9d1ab1cf701ae406b52
d_hound <james.whiteman@gmail.com>
2016-05-12 21:04:37 +1200

    first commit

---
+++ react-kanban/app/components/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+
+
+export default class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = NoteStore.getState();
+    }
+
+    componentDidMount() {
+        NoteStore.listen(this.storeChanged);
+    }
+    componentWillUnmount(){
+        NoteStore.unlisten(this.storeChanged);
+    }
+    storeChanged = (state) => {
+        this.setState(state);
+    };
+
+    render(){
+        const notes = this.state.notes;
+        return (
+            <div>
+                <button className='add-note' onClick={this.addNote}>+</button>
+                <Notes
+                    notes={notes}
+                    onEdit={this.editNote}
+                    onDelete={this.deleteNote}
+                    />
+            </div>
+        );
+    }
+
+    deleteNote(id, e){
+        e.stopPropagation();
+        NoteActions.delete(id);
+    };
+
+    addNote(){
+        NoteActions.create({task: 'new Task'});
+
+    };
+
+    editNote = (id,task) => {
+        if(!task.trim()){
+            return;
+        }
+        NoteActions.update({id,task});
+    };
+}
